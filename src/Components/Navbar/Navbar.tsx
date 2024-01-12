cba8ef3fc2369699c93931a957d8206e2cbb9f36
Dasun Thamash <tunathamash@gmail.com>
2024-01-12 23:34:17 +0530

    navbar added

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,11 +1,28 @@
 import React from "react"
 import './Navbar.css'
+import logo from "../Assets/logo.png"
+import cart_icon from "../Assets/cart_icon.png"
 
 const Navbar = ()=>{
     return(
-        <div>
-
+        <div className='navbar flex mt-30 mt-10 border-b pb-5'>
+            <div className='flex gap-2 ml-32 items-center'>
+                <img className='scale-75' src={logo} alt='logo'/>
+                <p className='font-bold text-3xl'>MODELER</p>
+            </div>
+            <ul className='nav-menu flex flex-row gap-10 mx-auto items-center font-bold'>
+                <li>Shop</li>
+                <li>Men</li>
+                <li>Women</li>
+                <li>Kids</li>
+            </ul>
+            <div className='nav-login-cart flex mr-32 gap-5'>
+                <button className=''>Login</button>
+                <img className='scale-50' src={cart_icon} alt='cart'/>
+                <div><div className='relative right-8 bg-red-600 bg-auto rounded-full'>0</div></div>
+            </div>
         </div>
+
     )
 }
 
