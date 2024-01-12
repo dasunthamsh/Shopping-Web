e19357dcaf883c631a8e09e3ef482db334f28f01
Dasun Thamash <tunathamash@gmail.com>
2024-01-13 02:31:34 +0530

    link path to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+          <Navbar/>
+          <Routes>
+              <Route path='/' element={<Shop/>}/>
+              <Route path='/mens' element={<ShopCategory />}/>
+              <Route path='/womens' element={<ShopCategory/>}/>
+              <Route path='/kids' element={<ShopCategory/>}/>
+              <Route path='/product' element={<Product/>}/>
+              <Route path=':productId' element={<Product/>}>
+              </Route>
+              <Route path='/cart' element={<Cart/>}/>
+              <Route path='/login' element={<LoginSignup/>}/>
+          </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import React, {useState} from "react"
 import './Navbar.css'
 import logo from "../Assets/logo.png"
 import cart_icon from "../Assets/cart_icon.png"
+import {Link} from "react-router-dom";
 
 const Navbar = ()=>{
 
@@ -15,14 +16,14 @@ const Navbar = ()=>{
                 <p className='font-bold text-3xl'>MODELER</p>
             </div>
             <ul className='nav-menu flex flex-row gap-10 mx-auto items-center font-bold'>
-                <li onClick={()=>{setMenu("Shop")}}>Shop{menu==="Shop" ? <hr style={{ backgroundColor: '#f00', height: '2px'}} />:null}</li>
-                <li onClick={()=>{setMenu("Men")}}>Men{menu==="Men" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>
-                <li onClick={()=>{setMenu("Women")}}>Women{menu==="Women" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>
-                <li onClick={()=>{setMenu("Kids")}}>Kids{menu==="Kids" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>
+                <li onClick={()=>{setMenu("Shop")}}><Link to='/'>Shop</Link>{menu==="Shop" ? <hr style={{ backgroundColor: '#f00', height: '2px'}} />:null}</li>
+                <li onClick={()=>{setMenu("Men")}}><Link to='/mens'>Men</Link>{menu==="Men" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>
+                <li onClick={()=>{setMenu("Women")}}><Link to='/womens'>Women</Link>{menu==="Women" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>
+                <li onClick={()=>{setMenu("Kids")}}><Link to='/kids'>Kids</Link>{menu==="Kids" ?<hr style={{ backgroundColor: '#f00', height: '2px'}}/>:null}</li>
             </ul>
             <div className='nav-login-cart flex mr-32 gap-5'>
-                <button className=''>Login</button>
-                <img className='scale-50' src={cart_icon} alt='cart'/>
+                <Link to='/login'><button className=''>Login</button></Link>
+                <Link to='/cart'><img className='scale-50' src={cart_icon} alt='cart'/></Link>
                 <div><div className='relative right-8 bg-red-600 bg-auto rounded-full'>0</div></div>
             </div>
         </div>
