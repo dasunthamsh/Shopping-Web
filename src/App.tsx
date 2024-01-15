193694d64060e6a06bee223b095e3048d103dc34
Dasun Thamash <tunathamash@gmail.com>
2024-01-16 01:51:43 +0530

    App.tsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,10 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
+
 
 function App() {
   return (
@@ -17,9 +21,9 @@ function App() {
           <Navbar/>
           <Routes>
               <Route path='/' element={<Shop/>}/>
-              <Route path='/mens' element={<ShopCategory />}/>
-              <Route path='/womens' element={<ShopCategory/>}/>
-              <Route path='/kids' element={<ShopCategory/>}/>
+              <Route path='/mens' element={<ShopCategory category='men' banner={men_banner}/>}/>
+              <Route path='/womens' element={<ShopCategory category='womens' banner={women_banner}/>}/>
+              <Route path='/kids' element={<ShopCategory category='kids' banner={kids_banner}/>}/>
               <Route path='/product' element={<Product/>}/>
               <Route path=':productId' element={<Product/>}>
               </Route>
