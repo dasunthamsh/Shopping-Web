20cd9a39d73f1f733a847b556279a73d0a637b02
Dasun Thamash <tunathamash@gmail.com>
2024-01-16 03:20:24 +0530

    loginSignUp page functions added

--- src/Pages/LoginSignup.tsx
+++ src/Pages/LoginSignup.tsx
@@ -3,7 +3,20 @@ import React from 'react'
 const LoginSignup = ()=>{
     return (
         <div>
-
+            <div>
+                <h1>Sign Up</h1>
+                <div>
+                    <input type='text' placeholder='your name'/>
+                    <input type='email' placeholder='your email'/>
+                    <input type='password' placeholder='your password'/>
+                </div>
+                <button>Continue</button>
+                <p>Already have an account? <span>Login here</span></p>
+                <div>
+                    <input type="checkbox" name='' id=''/>
+                    <p>By continuing, i agree to the terms of use & privacy policy</p>
+                </div>
+            </div>
         </div>
     )
 }
