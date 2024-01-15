8e8bd6b1becb7b5e70c1855b400bfc339649d12a
Dasun Thamash <tunathamash@gmail.com>
2024-01-16 02:23:47 +0530

    all_product are added

--- src/Context/ShopContext/ShopContext.tsx
+++ src/Context/ShopContext/ShopContext.tsx
@@ -1,24 +1,24 @@
 import React, { createContext, ReactNode } from 'react';
-import all_product from '../../Components/Assets/all_product';
+import all_product from "../../Components/Assets/all_product";
 
-interface ShopContextProps {
+interface ShopContextValue {
+    all_product: any[];
+}
+
+export const ShopContext = createContext<ShopContextValue | null>(null);
+
+interface ShopContextProviderProps {
     children: ReactNode;
 }
 
-interface ContextValue {
-    all_product: any; // You should replace 'any' with the actual type of 'all_product'
-}
-
-export const ShopContext = createContext<ContextValue | null>(null);
-
-const ShopContextProvider: React.FC<ShopContextProps> = (props) => {
-    const contextValue: ContextValue = { all_product };
+const ShopContextProvider = (props: ShopContextProviderProps) => {
+    const contextValue: ShopContextValue = { all_product };
 
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
         </ShopContext.Provider>
     );
-};
+}
 
 export default ShopContextProvider;
--- src/Pages/ShopCategory.tsx
+++ src/Pages/ShopCategory.tsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import { ShopContext } from '../Context/ShopContext/ShopContext'
+import dropdown_icon from '../Components/Assets/dropdown_icon.png'
+import all_product from '../Components/Assets/all_product';
+import {Item} from '../Components/Item/Item'
 
-const ShopCategory = ()=>{
-    return (
-        <div>
+interface ShopCategoryProps {
+    category?: string;
+    banner?:any;
 
-        </div>
-    )
 }
 
-export default ShopCategory
+const ShopCategory: React.FC<ShopCategoryProps> = ({ banner,category }) => {
+
+  //  const { all_product } = useContext(ShopContext);
+
+    return (
+        <div>
+            <img src={banner}/>
+            <div>
+                <p>
+                    <span>Showing 1-12</span> out of 36 products
+                </p>
+                <div>
+                    <img src={dropdown_icon}/>
+                </div>
+            </div>
+            <div>
+                {all_product.map((item,i) => {
+                    if (category==item.category){
+                        return <Item
+                            key={i}
+                            id={item.id}
+                            name={item.name}
+                            image={item.image}
+                            new_price={item.new_price}
+                            old_price={item.old_price}/>
+                    }else {
+                        return null;
+                    }
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default ShopCategory;
