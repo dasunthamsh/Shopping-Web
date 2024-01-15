3535f3a7cf776332e238a6dd46e81639d885ae16
Dasun Thamash <tunathamash@gmail.com>
2024-01-16 01:52:53 +0530

    index.tsx updated

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ShopContextProvider, {ShopContext} from "./Context/ShopContext/ShopContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <ShopContextProvider>
     <App />
-  </React.StrictMode>
+  </ShopContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
