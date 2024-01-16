7fd607bba959e68f7e974e62e575e03049ca451b
Dasun Thamash <tunathamash@gmail.com>
2024-01-16 12:40:26 +0530

    import Breadcrum.tsx and add and modify the code

--- src/Pages/Product.tsx
+++ src/Pages/Product.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import {ShopContext} from "../Context/ShopContext/ShopContext";
+import all_product from "../Components/Assets/all_product";
+import { useParams } from "react-router-dom"
+import Breadcrum from '../Components/Breadcrum/Breadcrum'
 
 const Product = ()=>{
+
+    //const {all_Products} = useContext(ShopContext);
+    const {productId} = useParams();
+    const product = all_product.find((e)=>e.id ===Number(productId));
+
     return (
         <div>
-
+            <Breadcrum product={product?.category ? product : { category: '', name: '' }} />
         </div>
     )
 }
